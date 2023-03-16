bc43d606f43bd009259e5468d5058b2ea7f52422
binhdv <binh.doan@sunteco.io>
2023-03-16 09:32:01 +0700

    chore: build sphinx project to nginx html image script
    Signed-off-by: binhdv <binh.doan@sunteco.io>

---
+++ README.md
@@ -0,0 +1,2 @@
+**Run command**
+> bash script.sh -p <path_to_docs_folder> -t <output_image_tag_name>
---
+++ script.sh
@@ -0,0 +1,44 @@
+# 1. build source ra html
+# - link tới docs folder: p
+# - tên của docker builder image : hardcode
+
+# 2. build docker image từ html
+# - tên output của docker image tag : t
+
+# 3. push lên docker hub
+# - link tới dockerhub repo
+
+while getopts p:t: flag
+do
+    case "${flag}" in
+        p) pathToDocs=${OPTARG};;
+        t) outputImageTag=${OPTARG};;
+    esac
+done
+
+echo "===> Path to docs folder: $pathToDocs";
+
+echo "";
+
+echo "===> Output image tag: $outputImageTag";
+
+echo "";
+
+echo "===> Build source code to html...";
+docker run --rm -v $pathToDocs:/docs binhdv37/sphinx-docs-builder make html;
+
+echo "";
+
+echo "===> Copy html folder to current folder";
+rm -rf html;
+cp -R $pathToDocs/build/html .;
+
+echo "";
+
+echo "===> Build docker image: $outputImageTag";
+docker build -f docs-dockerfile -t $outputImageTag .
+
+echo "";
+
+echo "===> Remove html folder";
+rm -rf html;
